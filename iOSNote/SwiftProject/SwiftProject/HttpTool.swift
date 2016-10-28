a78c7b062bc276a3b3810f45e86ebec3aaff6e61
wangdong <wangdong@ppzuche.com>
2016-10-28 22:38:25 +0800

    备份

--- iOSNote/SwiftProject/SwiftProject/HttpTool.swift
+++ iOSNote/SwiftProject/SwiftProject/HttpTool.swift
@@ -10,9 +10,25 @@ import Foundation
 
 class HttpTool: NSObject {
     
+    var callBack: ((jsonData: String) -> ())?
+    
     let dict: [String: AnyObject] = ["a":"b"]
     
     func loadData(callBack: (jsonData: String) -> ()) {
         
+        self.callBack = callBack
+        
+        dispatch_async(dispatch_get_global_queue(0, 0)) { 
+            print("发送网络请求\(NSThread.currentThread())")
+            
+            
+            dispatch_async(dispatch_get_main_queue(), { 
+                print("获取到数据,并且进行回调:\(NSThread.currentThread())")
+
+                callBack(jsonData: "jsonData数据")
+     
+            })
+            
+        }
     }
 }
